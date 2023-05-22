b8ee0303da801008d3b5f1dacdf4751c7203f877
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-05-22 17:34:36 +0600

    feat: Created ProtectedRoute component

---
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+
+interface Props extends React.PropsWithChildren {
+  isAllowed: boolean | null;
+}
+
+const ProtectedRoute: React.FC<Props> = ({ isAllowed, children }) => {
+  const router = useRouter();
+
+  if (!isAllowed) {
+    void router.push('/login');
+  }
+
+  return <>{isAllowed ? children : null}</>;
+};
+
+export default ProtectedRoute;
