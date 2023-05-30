9701e6ba620543d11f80d01d497822b7243832ee
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-05-30 14:51:52 +0600

    feat: Updated AddButton component

--- client/src/components/UI/AddButton/AddButton.tsx
+++ client/src/components/UI/AddButton/AddButton.tsx
@@ -1,18 +1,32 @@
-import AddIcon from '@mui/icons-material/Add';
 import { IconButton } from '@mui/material';
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 import style from './AddButton.module.css';
 
-interface Props {
+interface Props extends PropsWithChildren {
+  shouldRender: boolean;
+  background?: string;
   onClick?: () => void;
 }
 
-const AddButton: React.FC<Props> = ({ onClick }) => {
-  return (
-    <IconButton onClick={onClick} className={style.button}>
-      <AddIcon className={style.button__icon} />
-    </IconButton>
-  );
+const AddButton: React.FC<Props> = ({
+  shouldRender,
+  background,
+  onClick,
+  children,
+}) => {
+  if (shouldRender) {
+    return (
+      <IconButton
+        onClick={onClick}
+        className={style.button}
+        style={{ background }}
+      >
+        {children}
+      </IconButton>
+    );
+  }
+
+  return null;
 };
 
 export default AddButton;
