1d7626523fbb2426fcae9d0f645ecc27531a0974
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-06-01 18:46:54 +0600

    feat: Passed conditions on which actions render

--- client/src/features/Party/components/PartyItem.tsx
+++ client/src/features/Party/components/PartyItem.tsx
@@ -1,11 +1,13 @@
 import Card from '@/components/UI/Card/Card';
 import { selectParticipantJoining } from '@/dispatchers/participant/participantsSlice';
+import { gambleParticipants } from '@/dispatchers/participant/participantsThunk';
 import { deleteParty, getParties } from '@/dispatchers/party/partiesThunk';
+import useDispatchAction from '@/hooks/useDispatchAction';
 import useImageSrc from '@/hooks/useImageSrc';
 import useIsCreator from '@/hooks/useIsCreator';
 import useIsGambled from '@/hooks/useIsGambled';
 import useUserInParty from '@/hooks/useIsInParty';
-import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { useAppSelector } from '@/store/hooks';
 import { ApiParty } from '@/types';
 import { Button, Grid, Typography } from '@mui/material';
 import { useRouter } from 'next/router';
@@ -14,39 +16,48 @@ import useJoin from '../../../hooks/useJoin';
 
 interface Props {
   party: Pick<ApiParty, '_id' | 'title' | 'creator' | 'image'>;
+  isBanner?: boolean;
 }
 
-const PartyItem: React.FC<Props> = ({ party }) => {
-  const dispatch = useAppDispatch();
+const PartyItem: React.FC<Props> = ({ party, isBanner }) => {
   const joining = useAppSelector(selectParticipantJoining);
   const join = useJoin(party._id);
+  const gample = useDispatchAction(gambleParticipants(party._id));
   const isUserInParty = useUserInParty(party._id);
   const isUserCreator = useIsCreator(party._id);
   const isPartyGambled = useIsGambled(party._id);
   const partyImage = useImageSrc(party.image);
   const router = useRouter();
-
-  const onDelete = async () => {
-    await dispatch(deleteParty(party._id));
-    dispatch(getParties());
-  };
+  const onDelete = useDispatchAction(deleteParty(party._id), getParties());
 
   const onEdit = async (id: string) => {
     router.push(`parties/edit/${id}`);
   };
 
-  const cardActions = isUserCreator
-    ? [
-        { action: onDelete, title: 'Delete' },
-        { action: () => onEdit(party._id), title: 'Edit' },
-      ]
-    : undefined;
+  const cardActions = [
+    {
+      action: gample,
+      title: 'Gamble',
+      isHidden: isPartyGambled || !isUserInParty,
+    },
+    {
+      action: () => onEdit(party._id),
+      title: 'Edit',
+      isHidden: !isUserCreator,
+    },
+    {
+      action: onDelete,
+      title: 'Delete',
+      isHidden: !isUserCreator,
+    },
+  ];
 
   return (
     <Card
       image={partyImage}
       link={'parties/' + party._id}
       actions={cardActions}
+      width={isBanner ? '100%' : ''}
     >
       <Grid
         container
