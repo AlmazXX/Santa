c040db4f3d6133c2bd1f56bbe34fc038c6dea527
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-06-08 01:00:07 +0600

    refactor: Removed wishlist bar from a party page

--- client/src/pages/parties/[party].tsx
+++ client/src/pages/parties/[party].tsx
@@ -2,27 +2,16 @@ import Layout from '@/components/UI/Layout/Layout';
 import { getParticipants } from '@/dispatchers/participant/participantsThunk';
 import { selectSingleParty } from '@/dispatchers/party/partiesSlice';
 import { getSingleParty } from '@/dispatchers/party/partiesThunk';
-import { selectUser } from '@/dispatchers/user/usersSlice';
-import { openDrawer } from '@/dispatchers/wishlist/wishlistsSlice';
-import { getWishlist } from '@/dispatchers/wishlist/wishlistsThunk';
 import Participants from '@/features/Participant/Participants';
 import PartyItem from '@/features/Party/components/PartyItem';
-import WishlistBar from '@/features/Wishlist/WishlistBar';
-import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { useAppSelector } from '@/store/hooks';
 import { wrapper } from '@/store/store';
-import { User } from '@/types';
-import { Button, Grid } from '@mui/material';
+import { Grid } from '@mui/material';
 import { GetServerSideProps } from 'next';
 import React from 'react';
 
 const SingleParty: React.FC = () => {
-  const dispatch = useAppDispatch();
-  const user = useAppSelector(selectUser);
   const party = useAppSelector(selectSingleParty);
-
-  const openWishlist = () => {
-    dispatch(openDrawer());
-  };
 
   return (
     <Layout>
@@ -36,36 +25,16 @@ const SingleParty: React.FC = () => {
           <Participants />
         </Grid>
       </Grid>
-      {user && (
-        <>
-          <WishlistBar />
-          <Button
-            variant="contained"
-            onClick={openWishlist}
-            style={{
-              position: 'fixed',
-              left: '100%',
-              top: '50%',
-              transformOrigin: '0% 0%',
-              transform: 'rotate(270deg) translate(-50%,-96%)',
-            }}
-          >
-            Wishlist
-          </Button>
-        </>
-      )}
     </Layout>
   );
 };
 
 export const getServerSideProps: GetServerSideProps =
   wrapper.getServerSideProps((store) => async ({ params }) => {
-    const { user } = store.getState().users as { user: User };
     const { party } = params as { party: string };
 
     await store.dispatch(getSingleParty(party));
     await store.dispatch(getParticipants({ party }));
-    user && (await store.dispatch(getWishlist({ party, user: user._id })));
 
     return { props: {} };
   });
