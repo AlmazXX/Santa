5e5d1526353a93be232b0586247f42b97168bb42
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-06-09 00:27:38 +0600

    feat: Added conditinional participants rendering

--- client/src/features/Participant/Participants.tsx
+++ client/src/features/Participant/Participants.tsx
@@ -7,17 +7,25 @@ import ParticipantItem from './components/ParticipantItem';
 const Participants: React.FC = () => {
   const participants = useAppSelector(selectParticipants);
 
+  const participantsList = participants.length ? (
+    participants.map((participant) => (
+      <Grid item key={participant._id}>
+        <ParticipantItem participant={participant} />
+      </Grid>
+    ))
+  ) : (
+    <Grid item>
+      <Typography>No participants yet</Typography>
+    </Grid>
+  );
+
   return (
     <Grid container spacing={1}>
       <Grid item>
         <Typography>Participants</Typography>
       </Grid>
       <Grid item container direction="row" alignItems="stretch" spacing={2}>
-        {participants.map((participant) => (
-          <Grid item key={participant._id}>
-            <ParticipantItem participant={participant} />
-          </Grid>
-        ))}
+        {participantsList}
       </Grid>
     </Grid>
   );
