685a94a7364bbbd1943826843122e60b7f82de68
Almaz Choiubekov <choiubekov.almaz@gmail.com>
2023-06-10 12:44:40 +0600

    refactor: Moved chatBtn's styles to component

--- client/src/features/Chat/ChatButton.tsx
+++ client/src/features/Chat/ChatButton.tsx
@@ -1,21 +1,26 @@
-import { Button } from '@mui/material';
-import styles from './Chat.module.css';
-import React from 'react';
-import { useAppDispatch } from '@/store/hooks';
 import { openChat } from '@/dispatchers/chat/chatSlice';
+import { selectUser } from '@/dispatchers/user/usersSlice';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { Button } from '@mui/material';
+import React from 'react';
 
 const ChatButton: React.FC = () => {
   const dispatch = useAppDispatch();
+  const user = useAppSelector(selectUser);
 
   const onClick = () => {
     dispatch(openChat());
   };
 
-  return (
-    <Button variant="contained" onClick={onClick} className={styles.chatButton}>
+  return user ? (
+    <Button
+      variant="contained"
+      onClick={onClick}
+      style={{ position: 'absolute', bottom: '50px', right: '50px' }}
+    >
       Chat
     </Button>
-  );
+  ) : null;
 };
 
 export default ChatButton;
